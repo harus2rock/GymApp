ce68d4e99e1070ad185521740c0d51dbebe451ed
harus2rock <izumi@katolab.nitech.ac.jp>
2019-09-15 17:08:29 +0900

    Add function: Stop watch

--- app/src/main/java/com/example/gymapp/RunningActivity.java
+++ app/src/main/java/com/example/gymapp/RunningActivity.java
@@ -1,13 +1,34 @@
 package com.example.gymapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class RunningActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            count ++;
+            timerText.setText(dataFormat.format(count*period));
+            handler.postDelayed(this, period); // 100msec後にpost
+        }
+    };
+
+    private TextView timerText;
+    private SimpleDateFormat dataFormat = new SimpleDateFormat("mm:ss.S", Locale.US);
+
+    private int count, period;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,8 +36,21 @@ public class RunningActivity extends AppCompatActivity {
         // レイアウトファイル
         setContentView(R.layout.activity_running);
 
-        // ボタン
+        count = 0;
+        period = 100; // 100msec間隔
+
+        // textView タイマー表示箇所
+        timerText = findViewById(R.id.text_clock);
+        timerText.setText(dataFormat.format(0));
+
+        // ボタンたち
+        Button start = findViewById(R.id.button_start);
+        Button stop = findViewById(R.id.button_stop);
         Button back = findViewById(R.id.button_back);
+
+        // 押したとき
+        start.setOnClickListener(buttonClick);
+        stop.setOnClickListener(buttonClick);
         back.setOnClickListener(buttonClick);
     }
 
@@ -24,6 +58,22 @@ public class RunningActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
+                case R.id.button_start:
+                    // タイマーがすでに動いてるときは先に殺す
+                    if (count != 0) {
+                        handler.removeCallbacks(runnable);
+                        timerText.setText(dataFormat.format(0));
+                        count = 0;
+                    }
+                    handler.post(runnable);
+                    break;
+
+                case R.id.button_stop:
+                    handler.removeCallbacks(runnable);
+                    timerText.setText(dataFormat.format(0));
+                    count = 0;
+                    break;
+
                 case R.id.button_back :
                     finish();
                     break;
