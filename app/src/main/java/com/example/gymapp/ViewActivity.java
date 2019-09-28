288e8270a7d5882cf8d7c908796d644d7a6963c1
harus2rock <izumi@katolab.nitech.ac.jp>
2019-09-28 17:21:20 +0900

    Viewable database

--- app/src/main/java/com/example/gymapp/ViewActivity.java
+++ app/src/main/java/com/example/gymapp/ViewActivity.java
@@ -1,13 +1,20 @@
 package com.example.gymapp;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewActivity extends AppCompatActivity {
+
+    private RunningOpenHelper helper;
+    private SQLiteDatabase db;
+    private TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,6 +25,11 @@ public class ViewActivity extends AppCompatActivity {
         // ボタン
         Button back = findViewById(R.id.button_back);
         back.setOnClickListener(buttonClick);
+
+        // textview
+        textView = findViewById(R.id.text_view);
+
+        readData();
     }
 
     private View.OnClickListener buttonClick = new View.OnClickListener() {
@@ -30,4 +42,40 @@ public class ViewActivity extends AppCompatActivity {
             }
         }
     };
+
+    private void readData() {
+        if(helper == null){
+            helper = new RunningOpenHelper(getApplicationContext());
+        }
+
+        if(db == null){
+            db = helper.getReadableDatabase();
+        }
+
+        Cursor cursor = db.query(
+                "runningdb",
+                new String[] { "speed", "time"},
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i=0; i<cursor.getCount(); i++){
+            sbuilder.append(cursor.getFloat(0));
+            sbuilder.append(" : ");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("\n");
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        textView.setText(sbuilder.toString());
+    }
 }
